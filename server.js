89c440c42187bb9fafed180fd4cffe1bc43ecaba
meygaalex08 <133948123+meygaalex08@users.noreply.github.com>
2026-05-04 20:03:55 +1000

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ Your answer should be clear, concise, and actionable.
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
